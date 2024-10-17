0032b7698619eda3be76c7c0a1d65088fec612eb
MalinaDecrusch <malina.decrusch@gmx.de>
2024-10-17 11:17:48 +0200

    new file

---
+++ exercise0.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 17 09:07:05 2024
+
+@author: s_decrusch22
+"""
+
+p= 1
+
+i= 1
+
+t = 1
+
+"""print( x*(1+i)**p)"""
+
+n = 365*24*60
+
+print(p*(1+i/n)**(t*n))
+
+
+
+
+
